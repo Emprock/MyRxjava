14f042dceaf7769b043f1fae84cb06fefd558b08
John Doe <1198186839@qq.com>
2017-06-12 12:02:56 +0800

    test two

--- app/src/main/java/com/haoyan/myrxjava/MainActivity.java
+++ app/src/main/java/com/haoyan/myrxjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.haoyan.myrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        TextView tv_id= (TextView) findViewById(R.id.tv_id);
+        tv_id.setText("test");
     }
 }
